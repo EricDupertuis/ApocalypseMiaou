8bd30617e497dc757cbc45a598688f5289f2ea43
Eric Dupertuis <dupertuis.eric@gmail.com>
2017-01-20 14:59:07 +0100

    super fast incremental build

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,95 @@
-const gulp         = require('gulp');
-const eslint       = require('gulp-eslint');
-const plumber      = require('gulp-plumber');
-const browserify = require('browserify');
-const babelify = require('babelify');
-const source = require('vinyl-source-stream');
-const browserSync = require('browser-sync');
+"use strict";
 
-const DIST = 'dist/';
-const SRC = 'src/';
+const browserify = require("browserify"),
+    buffer       = require("vinyl-buffer"),
+    gulp         = require("gulp"),
+    path         = require("path"),
+    plumber      = require('gulp-plumber'),
+    source       = require("vinyl-source-stream"),
+    util         = require("gulp-util"),
+    watchify     = require("watchify"),
 
-gulp.task('build', function () {
-    return browserify({entries: SRC + 'app.js', debug: true})
-        .transform('babelify', {presets: ['babel-preset-es2015']})
-        .bundle()
-        .pipe(plumber())
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest(DIST));
-});
+    src = {
+        js:     ["./src/app.js"]
+    },
+    dest = {
+        js:     "./dist/"
+    };
 
-// create a task that ensures the `js` task is complete before
-// reloading browsers
-gulp.task('js-watch', ['build'], function (done) {
-    browserSync.reload();
-    done();
-});
+let bundlers;
 
-// use default task to launch Browsersync and watch JS files
-gulp.task('default', ['build'], function () {
+function bundles(profile) {
+    let start = new Date().getTime();
 
-    // Serve files from the root of this project
-    browserSync.init({
-        server: {
-            baseDir: "./"
+    if (bundlers === undefined) {
+        let opts = {},
+            presets = [];
+
+        if (profile == "prod") {
+            opts.debug = false;
+            presets.push("es2015");
+        } else {
+            opts.debug = true;
         }
-    });
 
-    // add browserSync.reload to the tasks array to make
-    // all browsers reload after tasks are complete.
-    gulp.watch("src/**/*.js", ['js-watch']);
+        bundlers = {};
+
+        for (let index in src.js) {
+            opts.standalone = "$";
+
+            switch (profile) {
+                case "watch":
+                    bundlers[src.js[index]] = watchify(browserify(src.js[index], opts)).transform("babelify", {presets: presets});
+                    break;
+
+                case "dev":
+                    bundlers[src.js[index]] = browserify(src.js[index], opts).transform("babelify", {presets: presets});
+                    break;
+
+                case "prod":
+                    bundlers[src.js[index]] = browserify(src.js[index], opts)
+                        .transform("babelify", {presets: presets})
+                        .transform({
+                            global: true
+                        }, "uglifyify");
+                    break;
+            }
+        }
+    }
+
+    for (let file in bundlers) {
+        bundle(file);
+    }
+}
+
+function bundle(file) {
+    let start = new Date().getTime(),
+        _ = bundlers[file]
+            .bundle()
+            .on("error", util.log.bind(util, "Browserify Error"))
+            .pipe(source(`${path.parse(file).name}.js`))
+            .pipe(buffer())
+            .pipe(gulp.dest(dest.js)),
+        time = new Date().getTime() - start;
+
+    util.log("[browserify] rebundle took ", util.colors.cyan(`${time} ms`), util.colors.grey(`(${file})`));
+
+    return _;
+}
+
+gulp.task("js:dev", bundles.bind(null, "dev"));
+gulp.task("js:prod", bundles.bind(null, "prod"));
+
+
+gulp.task("watch", function () {
+    bundles("watch");
+
+    for (let file in bundlers) {
+        bundlers[file].on("update", bundle.bind(null, file));
+    }
 });
+
+gulp.task("dev", ["js:dev"]);
+gulp.task("prod", ["js:prod"]);
+
+gulp.task("default", ["watch", "dev"]);
