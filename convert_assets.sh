8d45692d4818c897ff49bf0dec8e86390fa6df9b
Eric Dupertuis <dupertuis.eric@gmail.com>
2017-01-21 18:59:31 +0100

    remove the background copy

--- convert_assets.sh
+++ convert_assets.sh
@@ -5,10 +5,6 @@ set PROD assets/prod
 
 mkdir -p $RAW $PROD
 
-# Background image
-
-cp $RAW/background.jpg $PROD/background.jpg
-
 convert $RAW/characters/lion.png -resize 150x100 $PROD/characters/lion.png
 
 convert $RAW/enemies/helicopter.png -resize 200x150 $PROD/enemies/helicopter.png
