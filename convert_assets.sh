841a6dc9f761c3c9d45ba8c37f38374315e401b0
Eric Dupertuis <dupertuis.eric@gmail.com>
2017-01-22 09:49:40 +0100

    add excavator to convert assets

--- convert_assets.sh
+++ convert_assets.sh
@@ -13,6 +13,7 @@ convert $PROD/characters/lion.png $PROD/characters/tiger.png -append $PROD/chara
 convert $RAW/enemies/helicopter.png -resize 200x150 $PROD/enemies/helicopter.png
 convert $RAW/enemies/meteor.png -resize 288x150 -gravity East -background none -extent 288x150 $PROD/enemies/meteor.png
 convert $RAW/enemies/bucheron.png -resize 600x200 $PROD/enemies/bucheron.png
+convert $RAW/enemies/excavator.png -resize 1040x655 $PROD/enemies/excavator.png
 
 convert $RAW/effects/explosion.png -resize 2048x1024 $PROD/effects/explosion.png
 convert $RAW/effects/fireball.png -resize 159x32 -gravity East -background none -extent 159x32 $PROD/effects/fireball.png
